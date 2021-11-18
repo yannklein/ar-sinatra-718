698901b013bca9b51cffa8fd09acc5f28ecb075b
Yann Klein <yann.klein@me.com>
2021-11-18 21:31:03 +0900

    sinatra base

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
-require_relative "config/application"
-require "sinatra"
-require "sinatra/reloader"
-require "sinatra/activerecord"
+require_relative 'config/application'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
 
-get "/" do
-  "Hello world!"
+get '/' do
+  "Hello!"
 end
+
+# As a user I can list all the STUFFs
+# As a user I can see one STUFF's details
+# As a user I can add a STUFF
+
+# Ruby embedded into view files
+# pe + tab   ->    <%=  %>     (display something)
+# er + tab   ->    <%   %>     (display nothing)
